6f4d2779bc5481ab48f4374c67e34b787df09b7f
RES0136989 <leandro.tamashiro90@gmail.com>
2025-06-04 21:26:46 -0300

    Adicionando Veículo Adapter

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,18 +35,27 @@ android {
         jvmTarget = "11"
     }
     viewBinding{
-        enable=true
+        enable = true
     }
 }
 
 dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.14.0"))
+    implementation("com.google.firebase:firebase-auth:23.2.1")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.runtime.saved.instance.state)
+    implementation(libs.firebase.firestore.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("androidx.lifecycle:lifecycle-viewmodel:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
 }
--- app/src/main/java/br/com/codeschool/crud_automovel/view/LoginActivity.kt
+++ app/src/main/java/br/com/codeschool/crud_automovel/view/LoginActivity.kt
@@ -2,21 +2,22 @@ package br.com.codeschool.crud_automovel.view
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import br.com.codeschool.crud_automovel.databinding.ActivityLoginBinding
 import br.com.codeschool.crud_automovel.viewmodel.AutenticacaoViewModel
+import com.google.firebase.auth.FirebaseAuth
+
 
 //Apresenta tela de login do app
-class LoginActivity:AppCompatActivity() {
-    //vaiável para acessar os elementos da interface via ViewBinding
-    private lateinit var binding: ActivityLoginBinding //binding = vinculação
+class LoginActivity: AppCompatActivity() {
+    //Variável para acessar os elementos da interface via ViewBinding
+    private lateinit var binding: ActivityLoginBinding // binding = vinculacao
 
     //Variável para o ViewModel que gerencia a autenticação
-    private lateinit var viewModel: AutenticacaoViewModel //viewmodel - modelo de autenticação
+    private lateinit var viewModel: AutenticacaoViewModel //viewmodel -modelo de autenticação
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,35 +28,43 @@ class LoginActivity:AppCompatActivity() {
 
         configurarListeners()
         configurarObservers()
+
+        //Verificar se o usuário já está logado
+        if(FirebaseAuth.getInstance().currentUser != null){
+            navegarParaListaVeiculos()
+        }
     }
 
-    private fun configurarListeners(){
-        binding.botaoLogin.setOnClickListener(){
+    private fun configurarListeners() {
+        binding.botaoLogin.setOnClickListener() {
             val email = binding.editEmail.text.toString().trim()
             val senha = binding.editSenha.text.toString().trim()
-            if(validarEntradas(email, senha)){
+
+            if (validarEntradas(email, senha)) {
                 viewModel.login(email, senha)
             }
         }
-
-        binding.tvRegistrar.setOnClickListener(){
+        //Ir para tela de registro
+        binding.tvRegistrar.setOnClickListener() {
             startActivity(Intent(this, RegistroActivity::class.java))
         }
 
-        binding.tvEsqueciSenha.setOnClickListener(){
+        binding.tvEsqueciSenha.setOnClickListener {
             val email = binding.editEmail.text.toString().trim()
-            if(email.isNotEmpty()){
+            if (email.isNotEmpty()) {
                 viewModel.esqueciMinhaSenha(email)
             } else {
-                Toast.makeText(this, "Digite seu email para recuperar a senha.", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Digite seu email para recuperar a senha", Toast.LENGTH_SHORT)
+                    .show()
             }
         }
     }
-    private fun configurarObservers(){
-        viewModel.loading.observe(this){ estaCarregando ->
-            binding.barraDeProgresso.visibility = if(estaCarregando) View.VISIBLE else View.GONE
 
+    private fun configurarObservers() {
+        viewModel.loading.observe(this) { estaCarregando ->
+            binding.barraDeProgresso.visibility = if (estaCarregando) View.VISIBLE else View.GONE
         }
+
         viewModel.erro.observe(this) { erro ->
             erro?.let {
                 Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
@@ -63,15 +72,40 @@ class LoginActivity:AppCompatActivity() {
             }
         }
         viewModel.usuarioLogado.observe(this) { sucesso ->
-            if(sucesso){
-                irParaListaVeiculos()
+            if (sucesso) {
+                navegarParaListaVeiculos()
                 viewModel.limparSucessoLogin()
             }
         }
-        viewModel.resetSenhaSucesso.observe(this){ sucesso ->
-            if(sucesso){
 
+        viewModel.resetSenhaSucesso.observe(this) { sucesso ->
+            if (sucesso) {
+                Toast.makeText(
+                    this,
+                    "E-mail de recuperação de senha enviado com sucesso",
+                    Toast.LENGTH_SHORT
+                ).show()
+                viewModel.limparResetSenhaSucesso()
             }
         }
     }
-}
+
+    private fun validarEntradas(email: String, senha: String): Boolean {
+            var eValido = true
+
+            if (email.isEmpty()) {
+                binding.editEmail.error = "Campo obrigatório"
+                eValido = false
+            }
+
+            if (senha.isEmpty()) {
+                binding.editSenha.error = "Campo obrigatório"
+                eValido = false
+            }
+            return eValido
+        }
+        private fun navegarParaListaVeiculos(){
+            startActivity(Intent(this,ListaVeiculosActivity::class.java))
+            finish()
+        }
+    }
